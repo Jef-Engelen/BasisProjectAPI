ddd71ef242b55307a804411ba5df759a31111efd
Jef Engelen <r0850241@student.thomasmore.be>
2023-11-12 19:14:17 +0100

    a

--- app.py
+++ app.py
@@ -1,73 +1,87 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, Api, reqparse
-from flask_sqlalchemy import SQLAlchemy
+from fastapi import FastAPI, HTTPException, Body
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///items.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-api = Api(app)
+app = FastAPI()
 
-# Item model klasse
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+# CORS inschakelen
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# Aanmaken tabellen in database
-db.create_all()
+# SQL database opzetten
+DATABASE_URL = "sqlite:///groceries.db"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
-# Parser voor PUT en POST requests
-parser = reqparse.RequestParser()
-parser.add_argument('name', type=str, required=True)
+# Modellen aanmaken voor db
+class Grocery(Base):
+    __tablename__ = "groceries"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    brand = Column(String)
 
-class ItemListResource(Resource):
-    #GET endpoint
-    def get(self):
-        items = Item.query.all()
-        items_list = [{'id': item.id, 'name': item.name} for item in items]
-        return jsonify({'items': items_list})
+# Aanmaken tabellen
+Base.metadata.create_all(bind=engine)
 
-    #POST endpoint
-    def post(self):
-        args = parser.parse_args()
-        new_item = Item(name=args['name'])
-        db.session.add(new_item)
-        db.session.commit()
-        return jsonify({'item': {'id': new_item.id, 'name': new_item.name}}), 201
+# Klasse voor het aanmaken van een payload
+class GroceryCreate(BaseModel):
+    name: str
+    brand: str = None
 
-class ItemResource(Resource):
+# API routes
+@app.get("/api/groceries")
+def get_groceries():
+    with SessionLocal() as db:
+        groceries = db.query(Grocery).all()
+        return {"groceries": [{"id": grocery.id, "name": grocery.name, "brand": grocery.brand} for grocery in groceries]}
 
-    #GET endpoint
-    def get(self, item_id):
-        item = Item.query.get(item_id)
-        if item:
-            return jsonify({'item': {'id': item.id, 'name': item.name}})
+@app.post("/api/groceries")
+def create_grocery(grocery: GroceryCreate):
+    with SessionLocal() as db:
+        new_grocery = Grocery(**grocery.dict())
+        db.add(new_grocery)
+        db.commit()
+        db.refresh(new_grocery)
+        return {"grocery": {"id": new_grocery.id, "name": new_grocery.name, "brand": new_grocery.brand}}, 201
+
+@app.get("/api/groceries/{grocery_id}")
+def get_grocery(grocery_id: int):
+    with SessionLocal() as db:
+        grocery = db.query(Grocery).filter(Grocery.id == grocery_id).first()
+        if grocery:
+            return {"grocery": {"id": grocery.id, "name": grocery.name, "brand": grocery.brand}}
         else:
-            return jsonify({'message': 'Item not found'}), 404
+            raise HTTPException(status_code=404, detail="Grocery not found")
 
-    #DELETE endpoint
-    def delete(self, item_id):
-        item = Item.query.get(item_id)
-        if item:
-            db.session.delete(item)
-            db.session.commit()
-            return jsonify({'message': 'Item deleted'}), 200
+@app.delete("/api/groceries/{grocery_id}")
+def delete_grocery(grocery_id: int):
+    with SessionLocal() as db:
+        grocery = db.query(Grocery).filter(Grocery.id == grocery_id).first()
+        if grocery:
+            db.delete(grocery)
+            db.commit()
+            return {"message": "Grocery deleted"}
         else:
-            return jsonify({'message': 'Item not found'}), 404
+            raise HTTPException(status_code=404, detail="Grocery not found")
 
-    #PUT endpoint
-    def put(self, item_id):
-        args = parser.parse_args()
-        item = Item.query.get(item_id)
-        if item:
-            item.name = args['name']
-            db.session.commit()
-            return jsonify({'item': {'id': item.id, 'name': item.name}})
+@app.put("/api/groceries/{grocery_id}")
+def update_grocery(grocery_id: int, grocery_update: GroceryCreate = Body(...)):
+    with SessionLocal() as db:
+        grocery = db.query(Grocery).filter(Grocery.id == grocery_id).first()
+        if grocery:
+            grocery.name = grocery_update.name
+            grocery.brand = grocery_update.brand
+            db.commit()
+            return {"grocery": {"id": grocery.id, "name": grocery.name, "brand": grocery.brand}}
         else:
-            return jsonify({'message': 'Item not found'}), 404
-
-api.add_resource(ItemListResource, '/api/items')
-api.add_resource(ItemResource, '/api/items/<int:item_id>')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+            raise HTTPException(status_code=404, detail="Grocery not found")
